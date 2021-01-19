9a5910fce11a0e14f3ad4ee0df75a68718700c1e
pawanpatil636 <pawan10.patil@gmail.com>
2021-01-19 11:38:37 -0800

    first commit

--- pa.py
+++ pa.py
@@ -1,5 +1,6 @@
 num=11.223
 num1=2345.678
 num2=4567.3345454
-formatted_sum= "{1:.3f}".format(num,num1,num2)
-print(formatted_sum)
+num3=1111.0213
+sum= "{1:.4f}".format(num,num1,num2,num3)
+print(sum)
